c4ab3b4d58b1e019638aa2881dde49364704d56a
garethcmurphy <garethcmurphy@gmail.com>
2017-01-07 11:24:32 +0100

    add travis

--- cmake-build-debug/CMakeFiles/mpi-mhd-fv_impl.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libmpi-mhd-fv_impl.a"
-)
--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/gmurphy/Dropbox/ClionProjects/mpi-mhd-fv
-# Build directory: /Users/gmurphy/Dropbox/ClionProjects/mpi-mhd-fv/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "mpi-mhd-fv_test")
-subdirs(ext/gtest)
