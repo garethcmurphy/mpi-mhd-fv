482b777ec6f8b007e6da696d741e8d1dce75aa5c
Gareth Murphy <garethcmurphy@gmail.com>
2017-07-01 19:28:57 +0200

    Modifying view plot

--- see.py
+++ see.py
@@ -1,12 +1,27 @@
 import h5py
-f = h5py.File('hdf5_0010.h5','r')
+f = h5py.File('hdf5_0005.h5','r')
 import matplotlib 
-%matplotlib inline
-print f['Bx']
+print (f['Bx'])
 import matplotlib.pyplot as plt
 
+def printname(name):
+    print(name)
+
+f.visit(printname)
 bx=f['Bx']
-subplot(1,2,2)
-plt.subimage(f['Bx'])
-plt.subimage(f['By'])
+plt.figure()
+ax = plt.subplot(2,3,1)
+im=ax.pcolor(f['Velx'])
+ax = plt.subplot(2,3,2)
+im=ax.pcolor(f['Vely'])
+ax = plt.subplot(2,3,3)
+im=ax.pcolor(f['Bx'])
+ax = plt.subplot(2,3,4)
+im=ax.pcolor(f['By'])
+ax = plt.subplot(2,3,5)
+im=ax.pcolor(f['Density'])
+ax = plt.subplot(2,3,6)
+im=ax.pcolor(f['Energy'])
+plt.savefig('test_output.png', dpi=100)
+plt.show()
 
